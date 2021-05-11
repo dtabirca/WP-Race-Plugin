6ebc4e6b19110a1085c3598d57d3ff94476021bc
Daniel Tabirca <dtabirca@gmail.com>
2021-05-11 13:24:56 +0300

    0.1

--- admin/view.php
+++ admin/view.php
@@ -15,15 +15,17 @@
 		    <p>
 		    	<ul>
 		    		<li>
-		    			<input type="radio" checked name="racetype"> <label>Real Race (Normal/Wave Start,...)</label><br>
+		    			<input type="radio" checked name="racetype"> <label>Real Race (Mass/Wave/Individual Start,...)</label><br>
 		  				<input type="radio" name="racetype"> <label>Virtual Race (GPX Validation, ...)</label>
 		  			</li>
 		    		<li>...</li>  			
+		    		<li><input type="checkbox" name="team"> <label>Team Scoring</label></li>		  			
+		    		<li>...</li>  			
 		    		<li>Registration (requirements, limits)</li>
 		    		<li>...</li>
-		    		<li>Race time limits</li>
+		    		<li>Race time limits/penalties</li>
 		    		<li>...</li>    		
-		    		<li>Checkpoint definition</li>
+		    		<li>Laps, Intermediary Checkpoint definition</li>
 		    		<li>...</li>    		
 		    		<li>Units settings</li>
 		    		<li>...</li>
@@ -46,14 +48,14 @@
 			            <td>1</td>
 			            <td>101</td>
 			            <td>Zgăbeață Iftode</td>
-			            <td>40-49</td>
+			            <td>40-49 men</td>
 			            <td>yes</td>			            			            
 			        </tr>
 			        <tr>
 			            <td>2</td>
 			            <td>102</td>
 			            <td>John Doe</td>
-			            <td>18-29</td>
+			            <td>18-29 men</td>
 			            <td>yes</td>
 			        </tr>
 			    </tbody>
@@ -67,6 +69,7 @@
 			            <th>Bib</th>
 			            <th>Name</th>
 			            <th>Category</th>
+			            <th>Time</th>
 			            <th>Certificate</th>
 			        </tr>
 			    </thead>
@@ -75,14 +78,16 @@
 			            <td>1</td>
 			            <td>101</td>
 			            <td>Zgăbeață Iftode</td>
-			            <td>40-49</td>
+			            <td>40-49 men</td>
+			            <td>02:55:10</td>
 			            <td>link</td>			            			            
 			        </tr>
 			        <tr>
 			            <td>2</td>
 			            <td>102</td>
 			            <td>John Doe</td>
-			            <td>18-29</td>
+			            <td>18-29 men</td>
+			            <td>03:15:33</td>
 			            <td>link</td>
 			        </tr>
 			    </tbody>
@@ -98,7 +103,7 @@
 		    	<li>...</li>
 		    	<li>Export Results as csv/xls/pdf</li>
 		    	<li>...</li>
-		    	<li>Connect external services through API.</li>
+		    	<li>Connect external services through API (Live results).</li>
 		    	<li>...</li>		    	
 		    </ul>
 		</div>
